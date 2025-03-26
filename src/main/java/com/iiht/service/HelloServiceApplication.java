f36cdd1676c241526e83eace41c4d2a0625c0694
vmuser <vmuser@gmail.com>
2025-03-26 18:50:20 +0000

    vlnb

--- src/main/java/com/iiht/service/HelloServiceApplication.java
+++ src/main/java/com/iiht/service/HelloServiceApplication.java
@@ -16,6 +16,7 @@ import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 @EnableDiscoveryClient
