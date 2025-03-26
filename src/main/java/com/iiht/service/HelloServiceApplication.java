125b9d486b51d109a9c27c2c2d0a2705ee91dad6
Jagan K Surendran <93882353+Jagan1223@users.noreply.github.com>
2025-03-26 13:20:05 +0000

    updated vlnb

--- src/main/java/com/iiht/service/HelloServiceApplication.java
+++ src/main/java/com/iiht/service/HelloServiceApplication.java
@@ -3,13 +3,28 @@ package com.iiht.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class HelloServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServiceApplication.class, args);
+    }
 
+    @RestController
+    class HelloController {
+
+        @GetMapping("/hello")
+        public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+            return "Hello, " + sanitizeInput(name) + "!";
+        }
+
+        private String sanitizeInput(String input) {
+            return input.replaceAll("[^a-zA-Z0-9]", "");
+        }
+    }
 }
